72267adca3b0c5fa42b00e923a2afce474be14da
gunadermawan <gunadermawan.official@gmail.com>
2023-05-07 10:39:03 +0700

    create sample widget and update gradle dependency version

---
+++ app/src/main/java/com/gunder/widget/RandomGenerator.kt
@@ -0,0 +1,42 @@
+package com.gunder.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+import com.gunder.widget.utils.NumberGenerator
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class RandomGenerator : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val lastUpdate = "Random: " + NumberGenerator.generate(100)
+    val views = RemoteViews(context.packageName, R.layout.random_generator)
+    views.setTextViewText(R.id.appwidget_text, lastUpdate)
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
